3e4ba187fd9f66b8c568976bc1fe2a0846a12c97
MichaelKeeney <112013713+MichaelKeeney@users.noreply.github.com>
2024-03-12 17:25:09 -0500

    HawaiianPizza
    Added Hawaiian Pizza class with override methods

--- src/edu/mu/pizzaTypes/HawaiianPizza.java
+++ src/edu/mu/pizzaTypes/HawaiianPizza.java
@@ -1,43 +1,46 @@
-/**
- * 
- */
 package edu.mu.pizzaTypes;
 
-import java.util.List;
-
-import edu.mu.Cooking.ICookingStrategy;
 import edu.mu.pizza.AbstractPizza;
 import edu.mu.pizza.Toppings;
 
-/**
- * 
- */
 public class HawaiianPizza extends AbstractPizza{
-
+	
 	/**
 	 * @param toppingList
 	 * @param priceWithoutToppings
-	 * @param totalPrice
 	 * @param pizzaOrderID
-	 * @param cookingStrategy
-	 * @param cookingPrice
 	 */
-	public HawaiianPizza(List<Toppings> toppingList, double priceWithoutToppings, double totalPrice, int pizzaOrderID,
-			ICookingStrategy cookingStrategy, double cookingPrice) {
-		super(toppingList, priceWithoutToppings, totalPrice, pizzaOrderID, cookingStrategy, cookingPrice);
+	public HawaiianPizza(double priceWithoutToppings) {
+		super(3.00);
+		
 		// TODO Auto-generated constructor stub
+        addTopping(Toppings.CANADIAN_BACON);
+        addTopping(Toppings.CHEESE);
+        addTopping(Toppings.PINEAPPLE);
+        addToppingsToPrice(priceWithoutToppings);
 	}
 
 	@Override
-	protected double addTopingsToPrice(double priceWithoutToppings) {
-		// TODO Auto-generated method stub
-		return 0;
+	protected double addToppingsToPrice(double priceWithoutToppings) {
+		this.priceWithoutToppings = priceWithoutToppings;
+		
+		//set toppingPrice to 0
+		double toppingPrice = 0;
+		
+		//go through toppingList and add up the dollar values associated with each topping on pizza
+		for(Toppings topping: toppingList) {
+			toppingPrice = toppingPrice + topping.getToppingPrice();
+		}
+		//calculate totalPrice of pizza and toppings & return value
+		//should this be this.?
+		totalPrice = priceWithoutToppings + toppingPrice + cookingPrice;
+		return totalPrice;
 	}
 
 	@Override
 	public double updatePizzaPrice() {
 		// TODO Auto-generated method stub
-		return 0;
+		totalPrice += cookingPrice;
+		return totalPrice;
 	}
-
 }
