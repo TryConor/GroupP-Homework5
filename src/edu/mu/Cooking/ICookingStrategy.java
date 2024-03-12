129f2d1ba0da396e1fcf8c352a755afec6939a9b
kriswizzle03 <krislmor21@gmail.com>
2024-03-12 17:44:50 -0500

    Added comment about cooking strategy interface purpose/function.

--- src/edu/mu/Cooking/ICookingStrategy.java
+++ src/edu/mu/Cooking/ICookingStrategy.java
@@ -1,7 +1,7 @@
 package edu.mu.Cooking;
 
 import edu.mu.pizza.AbstractPizza;
-
+//cooking strategy interface for cooking strat classes to implement
 public interface ICookingStrategy {
 	public boolean cook(AbstractPizza pizza);
 }
