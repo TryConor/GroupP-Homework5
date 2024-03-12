dfdf2ce7d5f5f8ac85de78afab2895d0ae1527b8
Eli Wuerth <emwcd7@umsystem.edu>
2024-03-12 18:05:13 -0500

    updated PizzaCookingFactory
    added in the createPizza function and comments

--- src/edu/mu/Cooking/PizzaCookingFactory.java
+++ src/edu/mu/Cooking/PizzaCookingFactory.java
@@ -1,11 +1,44 @@
 package edu.mu.Cooking;
 
 import edu.mu.pizza.AbstractPizza;
+import edu.mu.pizzaTypes.HawaiianPizza;
+import edu.mu.pizzaTypes.MargheritaPizza;
 import edu.mu.pizzaTypes.PizzaType;
+import edu.mu.pizzaTypes.SupremePizza;
+import edu.mu.pizzaTypes.VegetarianPizza;
 
 public class PizzaCookingFactory {
-	public AbstractPizza createPizza(PizzaType pizzaType) {
-		return null;
+	private static int orderIDCounter = 0;
+	
+	/**
+	 * Creates a new pizza of the specified type.
+	 *
+	 * @param pizzaType The type of pizza to create.
+	 * @return A new pizza of the specified type.
+	 * @throws IllegalArgumentException If the specified pizza type is invalid.
+	 */
+	 public AbstractPizza createPizza(PizzaType pizzaType) {
+	     AbstractPizza pizza;
+	     
+	     
+	     switch (pizzaType) {
+	         case MARGHERITA:
+	             pizza = new MargheritaPizza(2.50);
+	             break;
+	         case VEGETARIAN:
+	             pizza = new VegetarianPizza(1.50);
+	             break;
+	         case HAWAIIAN:
+	             pizza = new HawaiianPizza(3.00);
+	             break;
+	         case SUPREME:
+	             pizza = new SupremePizza(3.50);
+	             break;
+	         default:
+	             throw new IllegalArgumentException("Invalid pizza type: " + pizzaType);
+		     }
 		
-	}
+		     pizza.setPizzaOrderID(++orderIDCounter);
+		     return pizza;
+		 }
 }
