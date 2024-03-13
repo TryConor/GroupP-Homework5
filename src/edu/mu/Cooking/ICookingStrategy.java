a548841d22b029b8b983341875fec7e38209c397
kriswizzle03 <krislmor21@gmail.com>
2024-03-12 22:34:18 -0500

    updated comments

--- src/edu/mu/Cooking/ICookingStrategy.java
+++ src/edu/mu/Cooking/ICookingStrategy.java
@@ -1,7 +1,8 @@
 package edu.mu.Cooking;
 
 import edu.mu.pizza.AbstractPizza;
-//cooking strategy interface for cooking strat classes to implement
+//cooking strategy interface for strategy classes to implement
 public interface ICookingStrategy {
+	//cooking strategy classes will use this method & override
 	public boolean cook(AbstractPizza pizza);
 }
